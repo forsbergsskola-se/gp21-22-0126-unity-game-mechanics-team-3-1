1cb4060d1b27d93ebd6449ae7c89a6825bbd9827
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-01-27 21:47:37 +0100

    Added: PlayerChargeJumpControllerHH.cs

---
+++ Team 3 Game Mechanics Project/Assets/Scripts/Harry H/PlayerChargeJumpControllerHH.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class PlayerChargeJumpControllerHH : MonoBehaviour
+{
+    private Rigidbody myRigidbody;
+    private PlayerInputControllerHH playerInputController;
+    private GroundCheckerHH groundChecker;
+    [SerializeField] private float minimumJumpForce = 100f;
+    [SerializeField] private float maximumJumpForce = 1000f;
+    [SerializeField] private float chargeTime = 1f;
+
+    private float jumpCharge;
+    
+    private void Awake()
+    {
+        myRigidbody = this.gameObject.GetComponent<Rigidbody>();
+        playerInputController = this.gameObject.GetComponent<PlayerInputControllerHH>();
+        groundChecker = this.gameObject.GetComponent<GroundCheckerHH>();
+    }
+
+    private void Update()
+    {
+        if (playerInputController.JumpInput)
+            jumpCharge += Time.deltaTime / chargeTime;
+
+        if (playerInputController.JumpInputUp)
+        {
+            var jumpForce = Mathf.Lerp(minimumJumpForce, maximumJumpForce, jumpCharge);
+            jumpCharge = 0f;
+
+            if (groundChecker.IsGrounded)
+                myRigidbody.AddForce(Vector3.up * jumpForce);
+        }
+    }
+}
