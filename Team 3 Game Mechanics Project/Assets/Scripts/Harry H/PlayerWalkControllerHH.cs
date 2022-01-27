6ad27762e38328faec966608fa75eef355fd3073
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-01-27 21:48:36 +0100

    Added logic to PlayerWalkControllerHH.cs

--- Team 3 Game Mechanics Project/Assets/Scripts/Harry H/PlayerWalkControllerHH.cs
+++ Team 3 Game Mechanics Project/Assets/Scripts/Harry H/PlayerWalkControllerHH.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerWalkControllerHH : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody myRigidbody;
+    private PlayerInputControllerHH playerInputController;
+    private GroundCheckerHH groundChecker;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float chargingMoveSpeedFactor = 0.5f;
+    
+    private void Awake()
     {
-        
+        myRigidbody = this.gameObject.GetComponent<Rigidbody>();
+        playerInputController = this.gameObject.GetComponent<PlayerInputControllerHH>();
+        groundChecker = this.gameObject.GetComponent<GroundCheckerHH>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //Slower move speed while charging a jump.
+        var currentMoveSpeed = moveSpeed;
+        if (playerInputController.JumpInput && groundChecker.IsGrounded)
+            currentMoveSpeed *= chargingMoveSpeedFactor;
+
+        myRigidbody.velocity = new Vector3(playerInputController.MoveInput * currentMoveSpeed, myRigidbody.velocity.y, 0);
     }
 }
