5f1d7dd5da2e09d13f730b0175c36a5d04608377
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-01-27 21:47:04 +0100

    Added: GroundCheckerHH.cs

---
+++ Team 3 Game Mechanics Project/Assets/Scripts/Harry H/GroundCheckerHH.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class GroundCheckerHH : MonoBehaviour
+{
+    public bool IsGrounded { get; private set; }
+    [SerializeField] private float groundCheckLength = 1f;
+    [SerializeField] private float groundCheckRadius = 0.5f;
+    [SerializeField] private LayerMask groundLayers;
+
+    private void Update()
+    {
+        var ray = new Ray(transform.position, Vector3.down);
+        IsGrounded = Physics.SphereCast(ray, groundCheckRadius, groundCheckLength, groundLayers);
+
+        // Debug.DrawRay(transform.position, Vector3.down * groundCheckLength, Color.magenta);
+    }
+
+    // private void OnDrawGizmos()
+    // {
+    //     Gizmos.DrawSphere(transform.position + Vector3.down * groundCheckLength, groundCheckRadius);
+    // }
+}
