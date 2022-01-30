6cb33ff5e3a24f45d22e966ff3ec1f7ba13a399e
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-01-30 22:24:38 +0100

    Added: ResetHH.cs

---
+++ Team 3 Game Mechanics Project/Assets/Scripts/Harry H/ResetHH.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetHH : MonoBehaviour
+{
+    public float deathZone = -5f;
+    public float respawnTime = 3f;
+    void Update()
+    {
+        if (this.gameObject.transform.position.y <= deathZone)
+        {
+            StartCoroutine(Reset());
+        }
+    }
+    private IEnumerator Reset()
+    {
+        yield return new WaitForSeconds(respawnTime);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
