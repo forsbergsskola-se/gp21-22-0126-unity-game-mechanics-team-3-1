e993b7bfd81c3c75bebd97676edd3a731e7610ea
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-01-31 10:23:13 +0100

    Create CommandContainerHH.cs

---
+++ Team 3 Game Mechanics Project/Assets/Scripts/Harry H/CommandContainerHH.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class CommandContainerHH : MonoBehaviour
+{
+    // these fields are visible in the inspector, which can be useful for testing.
+    // But in some cases we might want to use {HideInInspector} or getters/setters to hide these fields
+    
+    public float walkCommand;
+    public bool JumpCommandDown;
+    public bool JumpCommandUp;
+    public bool JumpCommand;
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
