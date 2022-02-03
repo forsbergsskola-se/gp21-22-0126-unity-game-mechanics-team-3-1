c215eaf3be4f8f410869e0e0c3d47b8f917399fb
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-02-03 14:58:59 +0100

    Updated: WalkController.cs with scriptable object references

--- Team 3 Game Mechanics Project/Assets/Scripts/Main/Movement/WalkController.cs
+++ Team 3 Game Mechanics Project/Assets/Scripts/Main/Movement/WalkController.cs
@@ -6,9 +6,11 @@ public class WalkController : MonoBehaviour
     private Rigidbody myRigidbody;
     private CommandContainer commandContainer;
     private GroundChecker groundChecker;
-    [SerializeField] private float moveSpeed = 5f;
-    [SerializeField] private float chargingMoveSpeedFactor = 0.5f;
-    
+    // [SerializeField] private float walkSpeed = 5f;
+    // [SerializeField] private float chargingWalkSpeedFactor = 0.5f;
+    [SerializeField] private WalkSpeedSO walkSpeedSO;
+
+
     private void Awake()
     {
         myRigidbody = this.gameObject.GetComponent<Rigidbody>();
@@ -24,9 +26,9 @@ public class WalkController : MonoBehaviour
     private void HandleWalking()
     {
         //Slower move speed while charging a jump.
-        var currentMoveSpeed = moveSpeed;
+        var currentMoveSpeed = walkSpeedSO.WalkSpeed;
         if (commandContainer.JumpCommand && groundChecker.IsGrounded)
-            currentMoveSpeed *= chargingMoveSpeedFactor;
+            currentMoveSpeed *= walkSpeedSO.ChargingWalkSpeedFactor;
 
         myRigidbody.velocity = new Vector3(commandContainer.walkCommand * currentMoveSpeed, myRigidbody.velocity.y, 0);
     }
