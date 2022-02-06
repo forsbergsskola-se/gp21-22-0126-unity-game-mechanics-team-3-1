20dee5ae1ab28e3fafe8108a59b346feaa062822
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-02-06 16:04:58 +0100

    New Script: UpgradeUnlocked.cs - Unlocks Better Dash Ability

---
+++ Team 3 Game Mechanics Project/Assets/Scripts/Personal Folders/Harry H/UpgradeUnlockHH.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+
+public class UpgradeUnlockHH : MonoBehaviour
+{
+    private DashHH dash;
+    private GameObject player;
+
+    private void Awake()
+    {
+        player = FindObjectOfType<PlayerIdentifier>().gameObject;
+        dash = player.GetComponent<DashHH>();
+    }
+
+    // Check if Player on collision, logic only executes if so
+    private void OnTriggerEnter(Collider other)
+    {
+        if (player == null)
+        {
+            return;
+        }
+
+        // Unlocks Air Dash if name matches
+        if (name != "Dash Skull") return;
+        dash.flyingDashUnlocked = true;
+        Debug.Log("UNLOCKED: Air Dash");
+        
+        // De-active this gameObject
+        gameObject.SetActive(false);
+    }
+}
