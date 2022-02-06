72a6b29b54d790a0b493549e26e1f08e08a08009
Pavel Sarmiento <pavel.sarmiento.kochersky@student.forsbergsskola.se>
2022-02-06 20:05:32 +0100

    initial code for grenade, also added green material for grenade prefab

--- Team 3 Game Mechanics Project/Assets/Scripts/Personal Folders/Pavel SK/GrenadePS.cs
+++ Team 3 Game Mechanics Project/Assets/Scripts/Personal Folders/Pavel SK/GrenadePS.cs
@@ -1,18 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GrenadePS : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    public float delay = 3f;
+    public GameObject explosionEffect;
+
+    float countdown;
+    bool hasExploded = false;
+
+    void Start(){
+        countdown = delay;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void Update(){
+        countdown -= Time.deltaTime;
+        if (countdown <= 0f && !hasExploded)
+        {
+            Explode();
+            hasExploded = true;
+        }
+    }
+
+    void Explode(){
+        Debug.Log("BOOM");
         
+        //show effect
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+
+        //get nearby objects
+        //Add force
+        //Damage
+        //Remove grenade
+        Destroy(gameObject);
     }
 }
