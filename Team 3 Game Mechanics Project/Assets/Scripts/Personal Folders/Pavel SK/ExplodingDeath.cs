2678c0487816a94162bf57c5b1993986023b91b4
Pavel Sarmiento <pavel.sarmiento.kochersky@student.forsbergsskola.se>
2022-02-07 21:45:08 +0100

    starting logic for explodingDeath

--- Team 3 Game Mechanics Project/Assets/Scripts/Personal Folders/Pavel SK/ExplodingDeath.cs
+++ Team 3 Game Mechanics Project/Assets/Scripts/Personal Folders/Pavel SK/ExplodingDeath.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 
 public class ExplodingDeath : MonoBehaviour{
     public Explode boom;
+    Health health;
 
 
     void Start(){
         boom = GetComponent<Explode>();
+        health = GetComponent<Health>();
     }
 
     void Update(){
         
         //When health 0 die
+        if (health.MaxHealth <= 0){
+            
+        }
         
         //When dead explode
             //deal dmg to anything
