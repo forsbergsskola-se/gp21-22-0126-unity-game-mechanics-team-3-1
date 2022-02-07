87adbd0c00c647af7686fe93dd2af56f6592259b
Pavel Sarmiento <pavel.sarmiento.kochersky@student.forsbergsskola.se>
2022-02-07 15:00:17 +0100

    Creating ExplodingDeath script

---
+++ Team 3 Game Mechanics Project/Assets/Scripts/Personal Folders/Pavel SK/ExplodingDeath.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodingDeath : MonoBehaviour{
+    public Explode boom;
+
+
+    void Start(){
+        boom = GetComponent<Explode>();
+    }
+
+    void Update(){
+        
+        //When health 0 die
+        
+        //When dead explode
+            //deal dmg to anything
+            
+    }
+}
