db6cda48da350f0296a7599ea046b15b66feab9f
Pavel Sarmiento <pavel.sarmiento.kochersky@student.forsbergsskola.se>
2022-02-07 01:30:18 +0100

    Finished super simple Grenade throwing script

--- Team 3 Game Mechanics Project/Assets/Scripts/Personal Folders/Pavel SK/GrenadeThrower.cs
+++ Team 3 Game Mechanics Project/Assets/Scripts/Personal Folders/Pavel SK/GrenadeThrower.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GrenadeThrower : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class GrenadeThrower : MonoBehaviour{
 
-    // Update is called once per frame
+    public float throwForce = 40f;
+    public GameObject grenadePrefab;
+    
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0)){
+            ThrowGrenade();
+        }
+    }
+
+    void ThrowGrenade(){
+        GameObject grenade = Instantiate(grenadePrefab, transform.position, transform.rotation);
+        Rigidbody rb = grenade.GetComponent<Rigidbody>();
+        rb.AddForce(transform.right * throwForce, ForceMode.VelocityChange);
     }
 }
