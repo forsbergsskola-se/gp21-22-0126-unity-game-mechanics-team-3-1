d6b35c6632780a6d2ef2380392e841edd6cd27f2
Pavel Sarmiento <pavel.sarmiento.kochersky@student.forsbergsskola.se>
2022-02-07 14:16:34 +0100

    Made Explode into its own working script

---
+++ Team 3 Game Mechanics Project/Assets/Scripts/Personal Folders/Pavel SK/Explode.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explode : MonoBehaviour
+{
+    
+    [SerializeField] float radius = 5f;
+    [SerializeField] float force = 700f;
+    [SerializeField] GameObject explosionEffect;
+    
+    
+    internal void ExplodeSomething(){
+        Debug.Log("BOOM");
+        
+        //show effect
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+        
+        //Get nearby objects
+        Collider[] collidersToDestroy = Physics.OverlapSphere(transform.position, radius);
+
+        foreach (Collider nearbyObject in collidersToDestroy)
+        {
+            
+            //destroy object
+            Destructible dest = nearbyObject.GetComponent<Destructible>();
+            if (dest != null){
+                dest.Destroy();
+            }
+        }
+        
+        //Get nearby objects
+        Collider[] collidersToMove= Physics.OverlapSphere(transform.position, radius);
+
+        foreach (Collider nearbyObject in collidersToMove){
+            
+            //Add blast force
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(force, transform.position, radius);
+            }
+        }
+        
+        //Remove grenade
+        Destroy(gameObject);
+    }
+}
