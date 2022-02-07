bbb9e19da0aa0a68d34b0ca21fcee2790b9dacb0
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-02-07 23:39:03 +0100

    Update UpgradeUnlocked.cs - updates Dash UI when upgrade is triggered

--- Team 3 Game Mechanics Project/Assets/Scripts/Personal Folders/Harry H/UpgradeUnlockHH.cs
+++ Team 3 Game Mechanics Project/Assets/Scripts/Personal Folders/Harry H/UpgradeUnlockHH.cs
@@ -1,15 +1,19 @@
 using System;
+using TMPro;
 using UnityEngine;
 
 public class UpgradeUnlockHH : MonoBehaviour
 {
     private DashHH dash;
     private GameObject player;
+    private GameObject plus;
 
     private void Awake()
     {
         player = FindObjectOfType<PlayerIdentifier>().gameObject;
         dash = player.GetComponent<DashHH>();
+        plus = GameObject.Find("+");
+        plus.SetActive(false);
     }
 
     // Check if Player on collision, logic only executes if so
@@ -25,6 +29,10 @@ public class UpgradeUnlockHH : MonoBehaviour
         dash.flyingDashUnlocked = true;
         dash.canDash = true;
         Debug.Log("UNLOCKED: Air Dash");
+        
+        // Adds a plus sign next to Dash UI to visualise upgrade
+        plus.SetActive(true);
+        
         
         // De-active this gameObject
         gameObject.SetActive(false);
