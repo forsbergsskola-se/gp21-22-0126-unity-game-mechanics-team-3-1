24c42c6ece04fc9be19b7a2a44672db5397e1832
Pavel Sarmiento <pavel.sarmiento.kochersky@student.forsbergsskola.se>
2022-02-07 01:00:20 +0100

    Finished Destructible script

--- Team 3 Game Mechanics Project/Assets/Scripts/Personal Folders/Pavel SK/Destructible.cs
+++ Team 3 Game Mechanics Project/Assets/Scripts/Personal Folders/Pavel SK/Destructible.cs
@@ -2,17 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Destructible : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Destructible : MonoBehaviour{
 
-    // Update is called once per frame
-    void Update()
-    {
+    public GameObject destroyedVersion;
+    
+    public void Destroy(){
+        Instantiate(destroyedVersion, transform.position, transform.rotation);
         
+        Destroy(gameObject);
     }
 }
