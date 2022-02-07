ebfeaaddc95c1f2ba5772b7d2953e3b9c69114ee
Pavel Sarmiento <pavel.sarmiento.kochersky@student.forsbergsskola.se>
2022-02-07 01:01:34 +0100

    seperated nearby object detection to Move and Destroy

--- Team 3 Game Mechanics Project/Assets/Scripts/Personal Folders/Pavel SK/GrenadePS.cs
+++ Team 3 Game Mechanics Project/Assets/Scripts/Personal Folders/Pavel SK/GrenadePS.cs
@@ -33,19 +33,30 @@ public class GrenadePS : MonoBehaviour
         //show effect
         Instantiate(explosionEffect, transform.position, transform.rotation);
         
-        //get nearby objects
-        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+        //Get nearby objects
+        Collider[] collidersToDestroy = Physics.OverlapSphere(transform.position, radius);
 
-        foreach (Collider nearbyObject in colliders)
+        foreach (Collider nearbyObject in collidersToDestroy)
         {
+            
+            //destroy object
+            Destructible dest = nearbyObject.GetComponent<Destructible>();
+            if (dest != null){
+                dest.Destroy();
+            }
+        }
+        
+        //Get nearby objects
+        Collider[] collidersToMove= Physics.OverlapSphere(transform.position, radius);
+
+        foreach (Collider nearbyObject in collidersToMove){
+            
             //Add blast force
             Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
             if (rb != null)
             {
                 rb.AddExplosionForce(force, transform.position, radius);
             }
-
-            //Damage
         }
         
         //Remove grenade
