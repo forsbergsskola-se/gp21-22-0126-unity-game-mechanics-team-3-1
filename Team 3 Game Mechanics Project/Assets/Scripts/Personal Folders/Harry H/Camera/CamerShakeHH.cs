a64e4ba65d5c31010d6f89db294cc39f411e656e
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-02-07 13:45:02 +0100

    New Script: CameraShakeHH.cs

---
+++ Team 3 Game Mechanics Project/Assets/Scripts/Personal Folders/Harry H/Camera/CamerShakeHH.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using UnityEngine;
+
+public class CamerShakeHH : MonoBehaviour
+{
+    private float elapsed;
+    private Vector3 originalPos;
+
+    public IEnumerator Shake(float duration, float magnitude)
+    {
+        elapsed = 0f;
+        originalPos = transform.localPosition;
+        while (elapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            transform.localPosition = new Vector3(x, y, originalPos.z);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        transform.localPosition = originalPos;
+    }
+}
