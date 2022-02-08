9628789947641f3f8c981765cf804ba39c0a45fe
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-02-08 01:34:34 +0100

    Update Dash2.cs with documentation

--- Team 3 Game Mechanics Project/Assets/Scripts/Personal Folders/Harry H/Dash2HH.cs
+++ Team 3 Game Mechanics Project/Assets/Scripts/Personal Folders/Harry H/Dash2HH.cs
@@ -7,7 +7,7 @@ public class Dash2HH : MonoBehaviour
     private float dashVelocity = 45f;
     private float dashTime;
     private float startDashTime = 0.2f;
-    private int direction;
+    private int dashDirection; 
     private void Awake()
     {
         rigidbody = GetComponent<Rigidbody>();
@@ -15,41 +15,46 @@ public class Dash2HH : MonoBehaviour
 
     private void Update()
     {
-        if (direction == 0)
+        // check if already dashing
+        if (dashDirection == 0)
         {
+            // use directional keycodes to assign a dash direction value
             if ((Input.GetKeyDown(KeyCode.A)) || (Input.GetKeyDown(KeyCode.LeftArrow)))
             {
-                direction = 1;
+                dashDirection = 1;
             }
             else if ((Input.GetKeyDown(KeyCode.D)) || (Input.GetKeyDown(KeyCode.RightArrow)))
             {
-                direction = 2;
+                dashDirection = 2;
             }
             else if ((Input.GetKeyDown(KeyCode.W)) || (Input.GetKeyDown(KeyCode.UpArrow)))
             {
-                direction = 3;
+                dashDirection = 3;
             }
             else if ((Input.GetKeyDown(KeyCode.S)) || (Input.GetKeyDown(KeyCode.DownArrow)))
             {
-                direction = 4;
+                dashDirection = 4;
             }
         }
         else
         {
+            // no longer dashing
             if (dashTime <= 0)
             {
-                direction = 0;
+                // resets
+                dashDirection = 0;
                 dashTime = startDashTime;
                 rigidbody.velocity = Vector3.zero;
             }
             else
             {
+                // slowly decrease dashTime until it reaches 0 so dash stops
                 dashTime -= Time.deltaTime;
                 
-                // switch expression: case 1 = rigidbody.velocity = Vector3.left * dashVelocity;
-                rigidbody.velocity = direction switch
+                // switch expression: assigns velocity based on current dash direction until dashTime = 0
+                rigidbody.velocity = dashDirection switch
                 {
-                    1 => Vector3.left * dashVelocity,
+                    1 => Vector3.left * dashVelocity, //rigidbody.velocity = Vector3.left * dashVelocity;
                     2 => Vector3.right * dashVelocity,
                     3 => Vector3.up * dashVelocity,
                     4 => Vector3.down * dashVelocity,
