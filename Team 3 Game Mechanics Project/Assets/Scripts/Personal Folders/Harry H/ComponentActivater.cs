83ccfd160bd96289b72885b88ef8d5675968d6b3
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-02-09 19:40:45 +0100

    Update ComponentActivater.cs - cleaned up, improved, documented

--- Team 3 Game Mechanics Project/Assets/Scripts/Personal Folders/Harry H/ComponentActivater.cs
+++ Team 3 Game Mechanics Project/Assets/Scripts/Personal Folders/Harry H/ComponentActivater.cs
@@ -3,20 +3,22 @@ public class ComponentActivater : MonoBehaviour
 {
    public string ScriptToToggle;
    private GameObject Player;
-   private Component component;
+   private Behaviour component;
    private void Awake()
    {
+      // Find and disable component specified in inspector
       Player = FindObjectOfType<PlayerIdentifier>().gameObject;
-      
-      var component = (Player.GetComponent(ScriptToToggle) as MonoBehaviour);
+      component = (Player.GetComponent(ScriptToToggle) as MonoBehaviour);
       component.enabled = false;
    }
 
    private void OnTriggerEnter(Collider other)
    {
+      // null ref return checks
       if (other.gameObject != Player) return;
+      if (component == null) return;
       
-      var component = (Player.GetComponent(ScriptToToggle) as MonoBehaviour);
+      // enable on trigger enter and deactivate this gameObject so script doesn't check on repeat trigger entries
       component.enabled = true;
       gameObject.SetActive(false);
    }
