67fc753c562351646ba820f3bfdbdc5b1ea9027f
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-02-09 18:36:39 +0100

    Added: New Script: ComponentActivater: activates player components when called

---
+++ Team 3 Game Mechanics Project/Assets/Scripts/Personal Folders/Harry H/ComponentActivater.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+public class ComponentActivater : MonoBehaviour
+{
+   public string ScriptToToggle;
+   private GameObject Player;
+   private Component component;
+   private void Awake()
+   {
+      Player = FindObjectOfType<PlayerIdentifier>().gameObject;
+      
+      var component = (Player.GetComponent(ScriptToToggle) as MonoBehaviour);
+      component.enabled = false;
+   }
+
+   private void OnTriggerEnter(Collider other)
+   {
+      if (other.gameObject != Player) return;
+      
+      var component = (Player.GetComponent(ScriptToToggle) as MonoBehaviour);
+      component.enabled = true;
+   }
+}
