983be77ca12ab219a150c684126d47c508f0d179
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-02-09 16:23:45 +0100

    Fixed my changes to Pavel's grenadethrower script so shoots right by default

--- Team 3 Game Mechanics Project/Assets/Scripts/Personal Folders/Pavel SK/GrenadeThrower.cs
+++ Team 3 Game Mechanics Project/Assets/Scripts/Personal Folders/Pavel SK/GrenadeThrower.cs
@@ -4,9 +4,15 @@ public class GrenadeThrower : MonoBehaviour{
 
     [SerializeField] float throwForce = 40f;
     [SerializeField] GameObject grenadePrefab;
+    private Vector3 walkDirection = Vector3.zero;
+    private Vector3 inputDirection;
     
     void Update()
     {
+        // Harry: added these vectors to judge if moving
+        inputDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0);
+        walkDirection = inputDirection;
+        
         if (Input.GetMouseButtonDown(0)){
             ThrowGrenade();
         }
@@ -16,7 +22,12 @@ public class GrenadeThrower : MonoBehaviour{
         GameObject grenade = Instantiate(grenadePrefab, transform.position, transform.rotation);
         Rigidbody rb = grenade.GetComponent<Rigidbody>();
         
-        // Harry changed from transform.Right to new vector 3 using horizontal input
-        rb.AddForce(new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0) * throwForce, ForceMode.VelocityChange);
+        // Harry: shoot direction defaults at right, then is based on input direction if moving
+        if (walkDirection == Vector3.zero)
+            rb.AddForce(transform.right * throwForce, ForceMode.VelocityChange);
+        else
+        {
+            rb.AddForce(inputDirection * throwForce, ForceMode.VelocityChange);
+        }
     }
 }
