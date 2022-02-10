57438a3bf25354622f8061aeb969cca165ba60fa
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-02-10 04:22:18 +0100

    Update ExplodingDeath.cs - removed unused references

--- Team 3 Game Mechanics Project/Assets/Scripts/Personal Folders/Pavel SK/ExplodingDeath.cs
+++ Team 3 Game Mechanics Project/Assets/Scripts/Personal Folders/Pavel SK/ExplodingDeath.cs
@@ -1,11 +1,7 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ExplodingDeath : MonoBehaviour{
-    public Collider enemyCollider;
-    public Collider playerCollider;
+    
     public Explode boom;
     Health health;
     
